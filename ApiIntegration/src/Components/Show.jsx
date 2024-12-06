bdc27eabe15e0012f31dd5ed027f38d506d420c4
ravi-sidharth <thenewravy@gmail.com>
2024-12-06 20:37:35 +0530

    update

--- ApiIntegration/src/Components/Show.jsx
+++ ApiIntegration/src/Components/Show.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
-import axios from 'axios'
-import { useState } from 'react'
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
 function Show() {
-    const [products,setProducts]=useState([])
-  const getProducts =()=> {
-    axios.get(`https://fakestoreapi.com/products`)
-    .then(product=> setProducts(product.data))
-    .catch(e=> console.log(e))
-  }
+  const [products, setProducts] = useState([]);
+  const getProducts = () => {
+    axios
+      .get(`https://fakestoreapi.com/products`)
+      .then((product) => setProducts(product.data))
+      .catch((e) => console.log(e));
+  };
+
   return (
     <>
-    <button onClick={getProducts} className='m-[5%] px-3 py-2 bg-green-600'>Get Product Data</button>
-   
-       <ul className='ml-[5%]' >
-      {products.length>0? products.map(product=><li key={product.id} className='bg-pink-400 rounded py-4 px-4 mt-4'>{product.title}</li>):<h1>Loading...</h1>}
-        </ul>
+      <button onClick={getProducts} className="m-[5%] px-3 py-2 bg-green-600">
+        Get Product Data
+      </button>
 
+      <ul className="ml-[5%]">
+        {products.length > 0 ? (
+          products.map((product) => (
+            <li key={product.id} className="bg-pink-400 rounded py-4 px-4 mt-4">
+              {product.title}
+            </li>
+          ))
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </ul>
     </>
-  )
+  );
 }
 
-export default Show
+export default Show;
